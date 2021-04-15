dc08e7279ae0b9cb15cdd596cb3476e607886ed3
S542301 <77767471+S542301@users.noreply.github.com>
2021-04-14 22:12:09 -0500

    commiting

--- routes/cars.js
+++ routes/cars.js
@@ -4,6 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', car_controlers.car_view_all_Page );
-
-
+router.get('/detail', car_controlers.car_view_one_Page);
+router.get('/create', car_controlers.car_create_Page);
+router.get('/update', car_controlers.car_update_Page);
+router.get('/delete', car_controlers.car_delete_Page);
 module.exports = router;
