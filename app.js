104a2f1129f7e749881c323f17c69dcc537c1f44
S542301 <77767471+S542301@users.noreply.github.com>
2021-04-23 18:53:35 -0500

    commiting

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const connectionString =process.env.MONGO_CON
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString,
-{useNewUrlParser: true, useUnifiedTopology: true});
+  { useNewUrlParser: true, useUnifiedTopology: true });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +17,9 @@ var carsRouter = require('./routes/cars');
 var starsRouter = require('./routes/stars');
 var slotRouter = require('./routes/slot');
 var resourceRouter = require('./routes/resource');
+var Account = require('./models/account');
+var palace = require("./models/palace");
+
 var app = express();
 
 // view engine setup
@@ -24,23 +30,63 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }));
+
+  
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/cars', carsRouter);
-app.use('/stars',starsRouter);
-app.use('/slot',slotRouter);
-app.use('/resource',resourceRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
+app.use('/resource', resourceRouter);
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// passport config
+// Use the existing connection
+// The Account model
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -51,40 +97,49 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-//Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded");
-});
-var car = require("./models/car");
+
 // We can seed the collection if needed on server start
-async function recreateDB(){
+async function recreateDB() {
   // Delete everything
   await car.deleteMany();
-  let instance1 = new car({carname:"Rolls Royce", brand:'BMW',
-  manufacturing_year:1971});
-  instance1.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("First object saved")
+  let instance1 = new car({
+    carname: "Rolls Royce", brand: 'BMW',
+    manufacturing_year: 1971
   });
-  
-  let instance2 = new car({carname:"Lincoln", brand:'Ford',
-  manufacturing_year:1917});
-  instance2.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("Second object saved")
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
   });
-  let instance3 = new car({carname:"kia", brand:'Hyundai',
-  manufacturing_year:1944});
-  instance3.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("Third object saved")
+
+  let instance2 = new car({
+    carname: "Lincoln", brand: 'Ford',
+    manufacturing_year: 1917
+  });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved")
+  });
+  let instance3 = new car({
+    carname: "kia", brand: 'Hyundai',
+    manufacturing_year: 1944
+  });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Third object saved")
   });
 }
 
 
 
-  let reseed = true;
-  if (reseed) { recreateDB();}
+let reseed = true;
+if (reseed) { recreateDB(); }
+
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded");
+});
+var car = require("./models/car");
